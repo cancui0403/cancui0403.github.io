a8330ca78f4031a1b2fe43c2c35dcae283c97a2c
cancui0403 <cancui0403@gmail.com>
2025-12-05 08:18:12 +0000

    Deploying to gh-pages from @ cancui0403/cancui0403.github.io@0446e706707e3ac92ca0718cc61e3e64962b8daa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -562,8 +562,8 @@ ninja.data = [{
             },},{id: "news-the-summer-after-sophomore-year-marked-a-milestone-i-joined-prof-kai-kang-s-group-and-began-my-research-journey-smile",
           title: 'The summer after sophomore year marked a milestone — I joined Prof. Kai...',
           description: "",
-          section: "News",},{id: "news-alignment-strategies-in-latent-space-models-theory-and-algorithms",
-          title: 'Alignment Strategies in Latent Space Models: Theory and Algorithms',
+          section: "News",},{id: "news-an-alignment-strategy-in-latent-space-models",
+          title: 'An Alignment Strategy in Latent Space Models',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
